f9e6b86432ad3bc61bc9b8e708d4529a3f4f43ed
Nurbolat Azhgaliev <nur9609@outlook.com>
2023-10-21 16:11:15 +0600

    добавила метод

--- Satbayev.University.DAL/Repository.cs
+++ Satbayev.University.DAL/Repository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,5 +29,55 @@ namespace Satbayev.University.DAL
                 return false;
             }
         }
+        public bool UpdateQuestion(QuestionTemplate question) 
+        { 
+            try
+            {
+                using (var db = new LiteDatabase(""))
+                {
+                    var col = (db.GetCollection<QuestionTemplate>());
+                        col.Update(question);
+                }
+                return true;
+            }
+            catch 
+            { 
+                return false; 
+            }
+        }
+        public bool DeleteQuestion(int questionId) 
+        {
+            try
+            {
+                using (var db = new LiteDatabase(""))
+                {
+                    var col = db.GetCollection<QuestionTemplate>();
+                    col.Delete(questionId);
+                    return true;
+                }               
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public List<QuestionTemplate> GetQuestions()
+        {
+            try
+            {
+                using (var db = new LiteDatabase(""))
+                {
+                    var col = db.GetCollection<QuestionTemplate>("QuestionTemplate");
+                    return col.FindAll().ToList();
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
+
 }
